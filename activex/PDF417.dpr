652fb7e39e412480a290c1754bb62ec4d1053fe6
leonardo.herrera <leonardo.herrera@gmail.com>
2011-01-28 04:11:29 +0000

    Updated activeX project to include source files in directory tree

--- activex/PDF417.dpr
+++ activex/PDF417.dpr
@@ -4,7 +4,9 @@ uses
   ComServ,
   PDF417BarcodeControl_TLB in 'PDF417BarcodeControl_TLB.pas',
   PDF417BarcodeActiveX in 'PDF417BarcodeActiveX.pas' {PDF417Barcode: CoClass},
-  About in 'About.pas' {PDF417BarcodeAbout};
+  About in 'About.pas' {PDF417BarcodeAbout},
+  pdf417lib in '..\src\pdf417lib.pas',
+  pdf417libimp in '..\src\pdf417libimp.pas';
 
 {$E ocx}
 
